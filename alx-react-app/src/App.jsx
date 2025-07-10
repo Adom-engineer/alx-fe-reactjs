59cef827d2888b78b09ee2a7f1d6929cf9f4099a
Emmanuel Boateng <eboateng1912@gmail.com>
2025-07-10 22:09:06 +0500

    Re-added WelcomeMessage component to App.jsx for checker

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,16 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
+
 
 
 function App() {
     return (
         <>
             <Header />
+            <WelcomeMessage />
             <MainContent />
             <Footer />
             <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
