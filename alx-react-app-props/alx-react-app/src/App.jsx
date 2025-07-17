cc81d23702ebf6ff1644c0747f9218ac0fff0e49
Emmanuel Boateng <eboateng1912@gmail.com>
2025-07-17 12:44:42 +0000

    Refactor prop drilling to use Context API for user data

---
+++ alx-react-app-props/alx-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Emmanuel Boateng", email: "eboateng1912@gmail.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
