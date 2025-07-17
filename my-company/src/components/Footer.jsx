a2558f064cd56f9baf526462b3b97a8647695662
Emmanuel Boateng <eboateng1912@gmail.com>
2025-07-17 16:55:13 +0000

    feat: build simple company website with React and React Router

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ marginTop: '30px', padding: '10px', background: '#eee', textAlign: 'center' }}>
+      <p>&copy; 2025 Our Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
