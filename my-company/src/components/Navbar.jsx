c711c17d5c13fade7ae5607eaa115ededa1e531f
Emmanuel Boateng <eboateng1912@gmail.com>
2025-07-17 17:03:45 +0000

    Added required inline styles to Navbar component

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const navbarStyle = {
+    backgroundColor: '#eee',          // required
+    display: 'flex',                  // required
+    justifyContent: 'space-between', // required
+    padding: '10px'
+  };
+
   return (
-    <nav style={{ padding: '10px', background: '#eee' }}>
-      <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-      <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
-      <Link to="/services" style={{ marginRight: '10px' }}>Services</Link>
-      <Link to="/contact">Contact</Link>
+    <nav style={navbarStyle}>
+      <div>
+        <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+        <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
+        <Link to="/services" style={{ marginRight: '10px' }}>Services</Link>
+      </div>
+      <div>
+        <Link to="/contact">Contact</Link>
+      </div>
     </nav>
   );
 }
