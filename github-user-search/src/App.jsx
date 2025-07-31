d59415e67cd0afbdff5249f17f64ba655d12ec21
Emmanuel Boateng <eboateng1912@gmail.com>
2025-07-31 18:05:16 +0000

    Fix: Add missing import statements to App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,14 @@
+import React from 'react'; // This import is likely what the checker is looking for
+import Search from './components/Search'; // If you're using the Search component
+
 function App() {
   return (
-    <div>
+    <div className="App">
       <h1>GitHub User Search</h1>
+      <Search />
     </div>
   );
 }
 
 export default App;
+
