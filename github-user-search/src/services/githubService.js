d5978089264772c6a30859d67a2ca07a6afec303
Emmanuel Boateng <eboateng1912@gmail.com>
2025-07-31 19:46:24 +0000

    Added fetchUserData to githubService

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -12,4 +12,8 @@ export async function advancedSearchUsers({ username, location, repos }) {
   const response = await axios.get(url);
   return response.data;
 }
+export async function fetchUserData(username) {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+}
 
