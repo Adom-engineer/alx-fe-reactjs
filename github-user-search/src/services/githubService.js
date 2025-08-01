689073d5c3a00d81b437cbe4f8f0ce64685d7c69
Emmanuel Boateng <eboateng1912@gmail.com>
2025-08-01 20:30:41 +0000

    Refactor advancedSearchUsers to use minRepos parameter

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,11 @@
-// src/services/githubService.js
 import axios from "axios";
 
 // Advanced search using GitHub Search API with pagination
-export async function advancedSearchUsers({ username, location, repos, page = 1 }) {
+export async function advancedSearchUsers({ username, location, minRepos, page = 1 }) {
   let query = username;
 
   if (location) query += `+location:${location}`;
-  if (repos) query += `+repos:>=${repos}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
 
   const url = `https://api.github.com/search/users?q=${query}&page=${page}`;
   const response = await axios.get(url);
